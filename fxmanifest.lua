6e0c38631aab751252bcff1f10aa5be47f6f94c2
yunglean_ <71585779+yunglean4171@users.noreply.github.com>
2023-07-10 20:02:12 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+games {'gta5'}
+
+fx_version 'cerulean'
+
+description 'yunglean_#4171 heal on kill'
+version '0.1.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_script {'server.lua'}
